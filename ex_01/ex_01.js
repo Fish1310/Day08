38d38656f6546486055636dd6f4eec5c97115020
Antoine Chopard <antoinechopard@mbp-de-antoine.home>
2024-10-09 19:55:30 +0200

    Finish ex01

---
+++ ex_01/ex_01.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  new Splide('#latest-creations-carousel', {
+    type       : 'loop',
+    perPage    : 3,
+    autoplay   : true,
+    interval   : 5000, // 5 secondes
+    focus      : 'center', // Met l'image centrale en avant
+  }).mount();
+});
